ac6f5491e2bba7e7ab95b5a50f8f97093843811b
waristo <waristo.dev@gmail.com>
2021-01-21 13:27:46 +0900

    backtracking: n과 M(2)

---
+++ java/src/p15650.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class p15650 {
+    static int n;
+    static int m;
+    static List<Integer> arr;
+    static boolean visited[];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new ArrayList<>();
+        visited = new boolean[n+1];
+
+        dfs(0,0);
+    }
+
+    private static void dfs(int k, int depth) {
+        if (depth == m){
+            for (int item : arr){
+                System.out.print(item + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = k + 1; i <= n; i++){
+            if (!visited[i]){
+                visited[i] = true;
+                arr.add(i);
+                dfs(i, depth + 1);
+                arr.remove(arr.size()-1);
+                visited[i] = false;
+            }
+        }
+    }
+}
