160c3205d62b8ca667ccecfe2b467b33a43b56d4
waristo <waristo.dev@gmail.com>
2021-01-21 13:44:13 +0900

    backtracking: n과 M(3)

---
+++ java/src/p15651.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class p15651 {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sb = new StringBuilder();
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        arr = new int[m];
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == m){
+            for (int item : arr){
+                sb.append(item + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++){
+            arr[depth] = i;
+            dfs(depth + 1);
+        }
+    }
+}
