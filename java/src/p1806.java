c436cd0749abbb2d49c819463687f48dc62359f3
waristo <waristo.dev@gmail.com>
2021-04-08 02:05:38 +0900

    부분합 수정

--- java/src/p1806.java
+++ java/src/p1806.java
@@ -5,23 +5,29 @@ public class p1806 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        int ans = Integer.MAX_VALUE;
+        int[] arr = new int[n+1];
+        int s = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < n; i++){
-            int sum = 0;
-            for (int j = i; j < n; j++){
-                sum += arr[j];
-                if (sum >= m){
-                    ans = Math.min(ans, j - i + 1);
-                    break;
-                }
+
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int ans = Integer.MAX_VALUE;
+
+        while(end <= n && start <= n){
+            if (sum >= s){
+                ans = Math.min(end - start, ans);
+                sum -= arr[start];
+                start++;
+            } else {
+                sum += arr[end];
+                end++;
             }
         }
         if (ans == Integer.MAX_VALUE) System.out.println(0);
