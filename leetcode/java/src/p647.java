cc02e506e465780cdb4ae4d0ffad0e349f0dfb16
waristo <waristo.dev@gmail.com>
2021-04-10 14:04:25 +0900

    refactoring

--- leetcode/java/src/p647.java
+++ leetcode/java/src/p647.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class p647 {
+class p647 {
+    public int countSubstrings(String s) {
+        int size = s.length();
+        int ans = 0;
+
+        for (int i = 0; i < size; i++){
+            for (int j = i+1; j <= size; j++){
+                if (isPalid(s.substring(i, j))){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    public boolean isPalid(String s){
+        int size = s.length();
+        for (int i = 0; i < size / 2; i++){
+            if (s.charAt(i) != s.charAt(size-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
