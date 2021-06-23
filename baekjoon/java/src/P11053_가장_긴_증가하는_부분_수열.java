c5da192eb71d021af5ddcf0a26bfa9af467a38bc
waristo <waristo.dev@gmail.com>
2021-06-23 17:19:32 +0900

    DP: 가장 긴 증갈하는 부분 수열

---
+++ baekjoon/java/src/P11053_가장_긴_증가하는_부분_수열.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class P11053_가장_긴_증가하는_부분_수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i < N; i++){
+            int x = arr[i];
+            for (int j = 0; j < i; j++){
+                int y = arr[j];
+                if (x > y) dp[i] = Math.max(dp[j] + 1, dp[i]);
+            }
+        }
+        int max = 0;
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
