39fcd30d45284292aa31a7daf647fb3671da268c
gimwonbae <waristo.dev@gmail.com>
2022-01-12 10:39:54 +0900

    Two Pointer : Container With Most Water

--- leetcode/golang/P11_Container_With_Most_Water.go
+++ leetcode/golang/P11_Container_With_Most_Water.go
@@ -1,24 +1,23 @@
 func maxArea(height []int) int {
-	ans := 0
-	left := 0
-	right := len(height) - 1
+    ans := 0
+    left := 0
+    right := len(height) - 1
 
     for left < right {
-		if height[left] < height[right] {
-			ans = max(ans, (right - left) * height[left])
-			left++
-		} else {
-			ans = max(ans, (right - left) * height[right])
-			right--
-		}
+	if height[left] < height[right] {
+	    ans = max(ans, (right - left) * height[left])
+	    left++
+	} else {
+	    ans = max(ans, (right - left) * height[right])
+	    right--
 	}
-	
-	return ans
+    }
+    return ans
 }
 
 func max(a int, b int) int {
-	if a < b {
-		return b
+    if a < b {
+	return b
     } else {
         return a
     }
